e07496dd834e5478ae605428cee8031e4e026c3a
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2025-03-11 14:14:13 +0530

    reseted

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-import SmartHomeDashboard from './Components/SmartHomeDashboard'
 
 function App() {
 
   return (
     <>
-      {/* <div className="flex justify-center items-center min-h-screen w-[100vw] bg-gray-100">
-      <div className="w-full max-w-md min-h-screen  w-[100vw] overflow-hidden border border-gray-200 rounded-3xl shadow-xl bg-white"> */}
-        <SmartHomeDashboard />
-      {/* </div>
-    </div> */}
+      
     </>
   )
 }
